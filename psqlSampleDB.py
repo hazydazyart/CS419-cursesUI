7984851c28165207b25fc7a4613f761a05c0445d
Megan <conleyme@onid.oregonstate.edu>
2015-11-21 14:19:15 -0800

    Code for sample psql movie db

---
+++ psqlSampleDB.py
@@ -0,0 +1,54 @@
+import psycopg2
+import sys
+
+
+con = None
+
+try:
+     
+    con = psycopg2.connect("dbname='postgres' user='postgres'")   
+    
+    cur = con.cursor()
+	
+	cur.execute("CREATE DATABASE movies WITH OWNER postgres")
+	
+	con.close()
+	
+	con = psycopg2.connect("dbname='movies' user='postgres'")
+  
+    cur.execute("CREATE TABLE action(Id INTEGER PRIMARY KEY, Name VARCHAR(20), Year INT)")
+    cur.execute("INSERT INTO action VALUES(1,'Die Hard',1988)")
+    cur.execute("INSERT INTO action VALUES(2,'Terminator 2',1991)")
+    cur.execute("INSERT INTO action VALUES(3,'Raiders of the Lost Ark',1981)")
+    cur.execute("INSERT INTO action VALUES(4,'Casino Royale',2006)")
+    cur.execute("INSERT INTO action VALUES(5,'The Avengers',2012)")
+    cur.execute("INSERT INTO action VALUES(6,'Taken', 2008)")
+    cur.execute("INSERT INTO action VALUES(7,'Aliens',1986)")
+    cur.execute("INSERT INTO action VALUES(8,'Star Wars',1977)")
+	
+	cur.execute("CREATE TABLE zombie(Id INTEGER PRIMARY KEY, Name VARCHAR(20), Year INT)")
+    cur.execute("INSERT INTO action VALUES(1,'Dawn of the Dead',1978)")
+    cur.execute("INSERT INTO action VALUES(2,'Night of the Living Dead',1968)")
+    cur.execute("INSERT INTO action VALUES(3,'28 Days Later',2002)")
+    cur.execute("INSERT INTO action VALUES(4,'Shaun of the Dead',2004)")
+    cur.execute("INSERT INTO action VALUES(5,'Day of the Dead',1985)")
+    cur.execute("INSERT INTO action VALUES(6,'Dead Snow', 2009)")
+    cur.execute("INSERT INTO action VALUES(7,'Resident Evil',2002)")
+    cur.execute("INSERT INTO action VALUES(8,'White Zombie',1932)")
+	
+	con.commit()
+    
+
+except psycopg2.DatabaseError, e:
+    
+    if con:
+        con.rollback()
+    
+    print 'Error %s' % e    
+    sys.exit(1)
+    
+    
+finally:
+    
+    if con:
+        con.close()
