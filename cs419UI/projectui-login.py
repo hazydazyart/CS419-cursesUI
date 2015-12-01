1b801cab53865daaab6e62e44ddd53dc80f45a9b
Megan <conleyme@onid.oregonstate.edu>
2015-11-30 21:55:20 -0800

    test

--- cs419UI/projectui-login.py
+++ cs419UI/projectui-login.py
@@ -323,7 +323,7 @@ class FetchUsersButton(npyscreen.ButtonPress):
 			cmd = """SELECT u.usename AS "User name", u.usesysid AS "User ID", CASE WHEN u.usesuper AND u.usecreatedb THEN CAST('superuser, create database' AS pg_catalog.text) WHEN u.usesuper THEN CAST('superuser' AS pg_catalog.text) WHEN u.usecreatedb THEN CAST('create database' AS pg_catalog.text) ELSE CAST('' AS pg_catalog.text) END AS "Attributes" FROM pg_catalog.pg_user u ORDER BY 1;"""
 			cur.execute(cmd)
 			rows = cur.fetchall()
-
+			print rows
 			self.parent.get_widget('viewusers').value = rows
 			self.parent.get_widget('viewusers').display()
 		except psycopg2.DatabaseError, e:
