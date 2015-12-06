617ada49ec36eb0cf7bc861eab8d439c5813a39f
Megan <conleyme@onid.oregonstate.edu>
2015-12-06 15:54:56 -0800

    restructuring repo

--- cs419UI/testcmd.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-
-cmd = """SELECT u.usename AS "User name", u.usesysid AS "User ID", CASE WHEN u.usesuper AND u.usecreatedb THEN CAST('superuser, create database' AS pg_catalog.text) WHEN u.usesuper THEN CAST('superuser' AS pg_catalog.text) WHEN u.usecreatedb THEN CAST('create database' AS pg_catalog.text) ELSE CAST('' AS pg_catalog.text) END AS "Attributes" FROM pg_catalog.pg_user u ORDER BY 1;"""
-con = psycopg2.connect(database='postgres', user='postgres')
-cur = con.cursor()
-cur.execute(cmd)
-data = cur.fetchall()
-print data
